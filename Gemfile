40dfa7d5c91f00c8e55c478c081fc38771e086e7
Raifc <raif.si@hotmail.com>
2024-11-15 20:42:40 -0300

    chore: Adds rubocop, configure simplecov and runs rubocop

--- Gemfile
+++ Gemfile
@@ -1,17 +1,19 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
 
-ruby "3.2.2"
+source 'https://rubygems.org'
+
+ruby '3.2.2'
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 7.2.2"
+gem 'rails', '~> 7.2.2'
 
 gem 'rack-cors'
 
 # Use postgresql as the database for Active Record
-gem "pg", "~> 1.1"
+gem 'pg', '~> 1.1'
 
 # Use the Puma web server [https://github.com/puma/puma]
-gem "puma", ">= 5.0"
+gem 'puma', '>= 5.0'
 
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 # gem "jbuilder"
@@ -26,16 +28,13 @@ gem "puma", ">= 5.0"
 # gem "bcrypt", "~> 3.1.7"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem "tzinfo-data", platforms: %i[ windows jruby ]
+gem 'tzinfo-data', platforms: %i[windows jruby]
 
 # Reduces boot times through caching; required in config/boot.rb
-gem "bootsnap", require: false
+gem 'bootsnap', require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
-
-# Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin Ajax possible
-# gem "rack-cors"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -43,11 +42,14 @@ group :development, :test do
   gem 'pry-byebug'
   gem 'pry-rails'
   gem 'rspec-rails'
-  gem 'rubocop', require: false
-  gem 'simplecov', require: false
+  gem 'rubocop', '~> 1.68'
+  gem 'rubocop-factory_bot', '~> 2.26', '>= 2.26.1'
+  gem 'rubocop-rails-omakase', '~> 1.0'
+  gem 'rubocop-rails', '~> 2.27'
+  gem 'rubocop-rspec', '~> 3.2'
+  gem 'rubocop-rspec_rails', '~> 2.30'
 end
 
 group :development do
-  # Speed up commands on slow machines / big apps [https://github.com/rails/spring]
-  # gem "spring"
+  gem 'simplecov', require: false
 end
--- spec/requests/api/v1/to_do_items_spec.rb
+++ spec/requests/api/v1/to_do_items_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
 
       expect(response).to be_successful
       items = JSON.parse(response.body)
-      
+
       expect(items.size).to eq(ToDoItem.count)
     end
   end
@@ -25,7 +25,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
 
         expect(response).to be_successful
         item = JSON.parse(response.body)
-        
+
         expect(item['id']).to eq(to_do_item.id)
       end
     end
@@ -45,7 +45,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
         expect {
           post '/api/v1/to_do_items', params: { to_do_item: valid_attributes }
         }.to change(ToDoItem, :count).by(1)
-        
+
         expect(response).to have_http_status(:created)
       end
     end
@@ -55,7 +55,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
         expect {
           post '/api/v1/to_do_items', params: { to_do_item: invalid_attributes }
         }.not_to change(ToDoItem, :count)
-        
+
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
@@ -67,7 +67,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
         put "/api/v1/to_do_items/#{to_do_item.id}", params: { to_do_item: { title: 'Updated Task' } }
 
         to_do_item.reload
-        
+
         expect(to_do_item.title).to eq('Updated Task')
         expect(response).to be_successful
       end
@@ -76,7 +76,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
     context 'with invalid parameters' do
       it 'does not update the to-do item and returns an unprocessable entity status' do
         put "/api/v1/to_do_items/#{to_do_item.id}", params: { to_do_item: invalid_attributes }
-        
+
         expect(response).to have_http_status(:unprocessable_entity)
       end
     end
@@ -87,7 +87,7 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
       expect {
         delete "/api/v1/to_do_items/#{to_do_item.id}"
       }.to change(ToDoItem, :count).by(-1)
-      
+
       expect(response).to have_http_status(:no_content)
     end
   end
@@ -137,8 +137,8 @@ RSpec.describe 'Api::V1::ToDoItemsController', type: :request do
   describe '#complete' do
     context 'when the to-do item exists' do
       it 'changes the status to complete' do
-        to_do_item = create(:to_do_item) 
-        
+        to_do_item = create(:to_do_item)
+
         put "/api/v1/to_do_items/#{to_do_item.id}/complete"
 
         expect(to_do_item.reload.status).to eq('complete')
